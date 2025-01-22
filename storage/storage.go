c496b762b58e24b3405fdf1a94f87aabb96e1077
Aix <Aix60689100@gmail.com>
2025-01-22 17:01:52 +0200

    Add BarOfTobacco struct for organizing strength and flavor data.

---
+++ storage/storage.go
@@ -0,0 +1,70 @@
+package storage
+
+type Storage interface {
+	TobaccoManager
+	FlavorManager
+	CatalogManager
+	GenerateMix
+}
+
+type FlavorManager interface {
+	AddFlavor(tobaccoName, flavorName string, flavorType Flavors) error
+	DeleteFlavor(tobaccoName, flavorName string) error
+	FlavorExists(tobaccoName string, flavorName string) bool
+	GetFlavors(tobaccoName string, strength Strength) (map[string]string, error)
+}
+
+type CatalogManager interface {
+	ShowTobaccoCatalog(tobaccoName string) string
+}
+
+type TobaccoManager interface {
+	AddTobacco(tobaccoName string, strength Strength) error
+	DeleteTobacco(tobaccoName string) error
+	TobaccoExists(tobaccoName string) bool
+	GetTobaccoBar(strength Strength) string
+}
+
+type GenerateMix interface {
+	GenerateMix(strength Strength, flavorType Flavors) string
+}
+
+type Strength string
+
+const (
+	Light  Strength = "Light"
+	Medium Strength = "Medium"
+	Strong Strength = "Strong"
+)
+
+type Flavors string
+
+const (
+	Sour      Flavors = "Sour"
+	Sweet     Flavors = "Sweet"
+	SourSweet Flavors = "SourSweet"
+)
+
+type BarOfTobacco struct {
+	Strength Strength
+	Flavor   map[Flavors][]string
+}
+
+func IsValidStrength(strength Strength) bool {
+	switch strength {
+	case Light, Medium, Strong:
+		return true
+	default:
+		return false
+	}
+}
+
+func IsValidFlavorType(flavorType Flavors) bool {
+	switch flavorType {
+	case Sour, Sweet, SourSweet:
+		return true
+	default:
+		return false
+	}
+
+}
